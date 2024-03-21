2d59e91c44dc70f4d6076c968a7a768a9069b243
Nyx <lilizhao107513@gmail.com>
2024-03-21 18:00:16 -0400

    I gave it my best shot, thank you

--- app/src/main/java/com/example/lilinyxzhao_horchatas_springbreakchooserapplication/MainActivity.kt
+++ app/src/main/java/com/example/lilinyxzhao_horchatas_springbreakchooserapplication/MainActivity.kt
@@ -8,13 +8,19 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -36,7 +42,7 @@ import com.example.lilinyxzhao_horchatas_springbreakchooserapplication.ui.theme.
 import java.util.Locale
 import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), TextToSpeech.OnInitListener{
     private var sensorManager: SensorManager? = null
     private var acceleration = 0f
     private var currentAcceleration = 0f
@@ -51,19 +57,49 @@ class MainActivity : ComponentActivity() {
 //    private var longitude: Double = 0.0
 //    private var latitude: Double = 0.0
     private lateinit var vacationSpotDetails: VacationSpot
+    private lateinit var textToSpeech: TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val locale = when (latestSelectedLanguage) {
+            "English" -> Locale.ENGLISH
+            //"Arabic" -> Locale("ar")
+            "Spanish" -> Locale("es")
+            "French" -> Locale.FRENCH
+            "Mandarin" -> Locale.CHINESE
+            else -> Locale.getDefault()
+        }
 
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+
+//                val locale = when (latestSelectedLanguage) {
+//                    "English" -> Locale.ENGLISH
+//                    "Arabic" -> Locale("ar")
+//                    "Spanish" -> Locale("es")
+//                    "French" -> Locale.FRENCH
+//                    "Mandarin" -> Locale.CHINESE
+//                    else -> Locale.getDefault()
+//                }
+                textToSpeech.language = locale
+                //textToSpeech.speak("مرحبًا", TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+            Log.d("SpeechRecognition", "Permission Granted")
         }
+
+
+
+
         editTextPhrase = findViewById(R.id.editTextPhrase);
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
         speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-                putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-                putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-            }
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, locale)
+        }
+
         val languageSelector = findViewById<ComposeView>(R.id.languageSelector)
         languageSelector.setContent {
             LiliNyxZhaoHorchatasSpringBreakChooserApplicationTheme {
@@ -100,17 +136,32 @@ class MainActivity : ComponentActivity() {
             sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
             SensorManager.SENSOR_DELAY_NORMAL)
     }
+    private fun isRecordAudioPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+    }
     @Deprecated("Deprecated in Java")
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startListening()
+        } else {
+            Toast.makeText(this, "Permission denied. The app cannot function without recording audio permission.", Toast.LENGTH_SHORT).show()
         }
+//        if (isRecordAudioPermissionGranted()) {
+//
+//            startListening()
+//        } else {
+//
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+//        }
     }
     private fun startListening() {
         val listener = object : RecognitionListener {
-            override fun onReadyForSpeech(params: Bundle?) {}
+            override fun onReadyForSpeech(params: Bundle?) {
+                Log.d("SpeechRecognition", "Ready")
+            }
             override fun onBeginningOfSpeech() {
+                Log.d("SpeechRecognition", "Begin")
                 runOnUiThread {
                     editTextPhrase.setText("")
                     editTextPhrase.hint = "Listening..."
@@ -118,8 +169,13 @@ class MainActivity : ComponentActivity() {
             }
             override fun onRmsChanged(rmsdB: Float) {}
             override fun onBufferReceived(buffer: ByteArray?) {}
-            override fun onEndOfSpeech() {}
-            override fun onError(error: Int) {}
+            override fun onEndOfSpeech() {
+                editTextPhrase.setText("")
+                Log.d("SpeechRecognition", "End")
+            }
+            override fun onError(error: Int) {
+                Log.d("SpeechRecognition", "Error: $error")
+            }
             override fun onResults(results: Bundle?) {
                 results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let {
                     if (it.isNotEmpty()) {
@@ -139,10 +195,6 @@ class MainActivity : ComponentActivity() {
         speechRecognizer.startListening(speechRecognizerIntent)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        speechRecognizer.destroy()
-    }
 
     private val sensorListener: SensorEventListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent) {
@@ -160,6 +212,19 @@ class MainActivity : ComponentActivity() {
         }
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+//            val results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+//            if (results != null && results.isNotEmpty()) {
+//                val spokenText = results[0]
+//                Log.d("SpeechRecognition", "Recognized Text: $spokenText")
+//                editTextPhrase.setText(spokenText)
+//            }
+//        }
+//    }
+
 
 //    private fun launchGoogleMapsForVacationSpot(selectedLanguage: String, longitude: Double, latitude: Double) {
 //        // Launch Google Maps with geo URI for the determined vacation spot
@@ -183,8 +248,40 @@ class MainActivity : ComponentActivity() {
         val gmmIntentUri = "geo:${vacationSpotDetails.latitude},${vacationSpotDetails.longitude}?q=${vacationSpotDetails.name}"
         val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(gmmIntentUri))
         mapIntent.setPackage("com.google.android.apps.maps")
-        startActivity(mapIntent)
+        //startActivity(mapIntent)
+
+        val helloText = when (selectedLanguage) {
+            "English" -> "Hello"
+            //"Arabic" -> "\u0645\u0631\u062D\u0628\u064B\u0627"
+            //"مرحبًا"
+            "Spanish" -> "Hola"
+            "French" -> "Bonjour"
+            "Mandarin" -> "你好"
+            else -> "Hello"
+        }
+
+        // Set up Text-to-Speech to speak the translated "Hello" text
+        textToSpeech.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                Log.d("launchGoogleMaps", "Started Speeking")
+            }
+
+            override fun onDone(utteranceId: String?) {
+                startActivity(mapIntent)
+            }
+
+            @Deprecated("Deprecated in Java")
+            override fun onError(utteranceId: String?) {
+                Log.d("launchGoogleMaps", "error: $utteranceId")
+            }
+        })
+
+
+        val params = HashMap<String, String>()
+        params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "helloUtterance"
+        textToSpeech.speak(helloText, TextToSpeech.QUEUE_FLUSH, params)
     }
+
 
     private fun getVacationSpotForLanguage(language: String): List<VacationSpot> {
         return when (language) {
@@ -194,11 +291,11 @@ class MainActivity : ComponentActivity() {
                 VacationSpot("New York City", 40.7128, -74.0060),
                 VacationSpot("Sydney", -33.8688, 151.2093)
             )
-            "Arabic" -> listOf(
-                VacationSpot("Cairo", 30.0330, 31.2336),
-                VacationSpot("Dubai", 25.276987, 55.296249),
-                VacationSpot("Istanbul", 41.0082, 28.9784)
-            )
+//            "Arabic" -> listOf(
+//                VacationSpot("Cairo", 30.0330, 31.2336),
+//                VacationSpot("Dubai", 25.276987, 55.296249),
+//                VacationSpot("Istanbul", 41.0082, 28.9784)
+//            )
             "Spanish" -> listOf(
                 VacationSpot("Mexico City", 19.4326, -99.1332),
                 VacationSpot("Barcelona", 41.3851, 2.1734),
@@ -218,11 +315,29 @@ class MainActivity : ComponentActivity() {
             else -> listOf(VacationSpot("Default Location", 0.0, 0.0))
         }
     }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Text-to-Speech engine initialization successful
+            val locale = Locale.getDefault() // Default locale, can be changed based on user preference
+            textToSpeech.language = locale
+        } else {
+            // Text-to-Speech engine initialization failed
+            Log.e("TextToSpeech", "Initialization failed")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer.destroy()
+        textToSpeech.stop()
+        textToSpeech.shutdown()
+    }
 }
 
 @Composable
 fun LanguageSelector(onLanguageSelected: (String, VacationSpot) -> Unit, getVacationSpotForLanguage: (String)->List<VacationSpot>) {
-    val languages = listOf("English", "Arabic", "Spanish", "French", "Mandarin")
+    val languages = listOf("English", "Spanish", "French", "Mandarin")
     var expanded by remember { mutableStateOf(false) }
     var selectedLanguage by remember { mutableStateOf(languages[0]) }
 
@@ -232,7 +347,7 @@ fun LanguageSelector(onLanguageSelected: (String, VacationSpot) -> Unit, getVaca
     ) {
         Column {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Text("Choose Language: $selectedLanguage", modifier = Modifier.padding(end = 8.dp))
+                Text("Choose Language")
                 IconButton(onClick = { expanded = !expanded }) {
                     Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Language Options")
                 }
